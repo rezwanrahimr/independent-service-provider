c5e63c3dcb46426aa68ad5acc3f865ac58d447cd
rezwanrahimr <rezwanrahim69@gmail.com>
2022-04-18 14:03:34 +0600

    loading problem solved

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import { auth } from '../../firebase.init';
+import Loading from '../Loading/Loading';
 
 const RequredAuth = ({children}) => {
-    const [user] = useAuthState(auth);
+    const [user,loading] = useAuthState(auth);
     const location = useLocation();
-    if(!user){
+    if(loading){
+      return <Loading></Loading>
+    }
+    else if(!user){
       return <Navigate to='/login' state={{from: location}} replace></Navigate>  
     }
     return children;
